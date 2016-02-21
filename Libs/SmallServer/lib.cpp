e419abc3d271b80732cf0231c7cb5811e078a791
brakmic <brakmic@gmail.com>
2016-02-21 16:42:19 +0100

    added link & comment in lib.cpp

--- Libs/SmallServer/lib.cpp
+++ Libs/SmallServer/lib.cpp
@@ -3,12 +3,15 @@
 #include <hpx/runtime/components/component_factory.hpp>
 #include "smallserver.h"
 
+//original code: http://stellar.cct.lsu.edu/files/hpx-0.9.11/html/hpx/manual/init/configuration/loading_components.html#hpx.manual.init.configuration.loading_components.component_example
+//adapted to work under Visual Studio
+
 using server_type = hpx::components::component<mylib::components::server>;
 
 // Define boilerplate required once per component module.
 HPX_REGISTER_COMPONENT_MODULE();
 
-// Define factory object associated with our component of type 'app::server'.
+// Define factory object associated with our component of type 'app_server'.
 HPX_REGISTER_COMPONENT(server_type, app_server);
 
 // Define boilerplate code required for each of the component actions. Use the
